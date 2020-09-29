09a2b9151e04a3e4c9b77ee19f1efa147f07d10a
svedrine <sebastien94170@gmail.com>
2020-09-29 23:54:29 +0200

    Add gif search

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
+const https = require('https');
 const { Client, MessageAttachment, MessageEmbed  } = require('discord.js');
-
 const client = new Client();
 
 // READY EVENT
@@ -12,13 +12,39 @@ client.on('ready', () => {
 
 // MESSAGE EVENT
 client.on('message', message => {
+    // SPLIT SENTENCE INTO WORDS
+    const words = message.content.split(' ');
+
     // WHAT'S MY AVATAR
     if (message.content === 'Avatar') {
       message.reply(message.author.displayAvatarURL());
     }
 
     // SEND A GIF
+    if (words.length >= 2 && words[0] === 'gif') {
+      // USER SEARCH
+      const gif_search = message.content.replace(words[0], '');
 
+      https.get(`https://api.giphy.com/v1/gifs/search?q=${gif_search}&api_key=${process.env.GIPHY_API_TOKEN}&limit=1`, (response) => {
+        // SET ENCODING OF RESPONSE TO UTF8
+        response.setEncoding('utf8');
+        let body = '';
+        // LISTENS FOR THEN EVENT OF THE DATA BUFFER AND STREAM
+        response.on('data', (d) => {
+            // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+              body += d;
+        });
+        
+        response.on('end', () => {
+            // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+            let parsed = JSON.parse(body);
+
+            gif = parsed.data.shift();
+
+            message.channel.send(gif.images.original.url);
+        });
+      });
+    }
 
     // TRANSLATE MESSAGE
 
